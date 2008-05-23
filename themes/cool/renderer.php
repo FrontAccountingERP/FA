0d469b61b73c9fff2c428eb042d346587f62aac9
Joe Hunt <joe.hunt.consulting@gmail.com>
2008-05-23 14:15:14 +0000

    Moved menu-system from header.inc/footer.inc to renderer.php for relevant theme. Fixed frontaccounting.php to get the correct renderer.php.

--- themes/cool/renderer.php
+++ themes/cool/renderer.php
@@ -1,31 +1,114 @@
 <?php
 
-	class renderer 
+	class renderer
 	{
-		function wa_header() 
+		function wa_header()
 		{
 			page(_("Main Menu"), false, true);
 		}
 
-		function wa_footer() 
+		function wa_footer()
 		{
 			end_page(false, true);
 		}
 
-		function menu_header(&$menu) 
+		function menu_header($title, $no_menu, $is_index)
 		{
+			global $path_to_root, $applications, $help_base_url, $db_connections;
+			// you can owerride the table styles from config.php here, if you want.
+			//global $table_style, $table_style2;
+			//$table_style 	= "cellpadding=3 border=1 bordercolor='#8cacbb' style='border-collapse: collapse'";
+			//$table_style2 = "cellpadding=3 border=1 bordercolor='#cccccc' style='border-collapse: collapse'";
+			echo "<table class='callout_main' border='0' cellpadding='0' cellspacing='0'>\n";
+			echo "<tr>\n";
+			echo "<td colspan='2' rowspan='2'>\n";
+
+			echo "<table class='main_page' border='0' cellpadding='0' cellspacing='0'>\n";
+			echo "<tr>\n";
+			echo "<td>\n";
+			echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+			echo "<tr>\n";
+			echo "<td class='quick_menu'>\n";
+			if (!$no_menu)
+			{
+				$local_path_to_root = $path_to_root;
+				$sel_app = $_SESSION['sel_app'];
+				echo "<table cellpadding=0 cellspacing=0 width='100%'><tr><td>";
+				echo "<div class=tabs>";
+				foreach($applications as $app => $name)
+				{
+					echo "<a ".($sel_app == $app ? "class='selected' " : "").
+					"href='$local_path_to_root/index.php?application=".$app.
+						SID ."'>" .$name . "</a>";
+				}
+				echo "</div>";
+
+				echo "</td></tr></table>";
+
+				echo "<table class=logoutBar>";
+				echo "<tr><td class=headingtext3>" . $db_connections[$_SESSION["wa_current_user"]->company]["name"] . " | " . $_SERVER['SERVER_NAME'] . " | " . $_SESSION["wa_current_user"]->name . "</td>";
+				if ($help_base_url != null)
+				{
+					echo "<td align=right ><a target = '_blank' onclick=" .'"'."javascript:openWindow(this.href,this.target); return false;".'" '. "href='". help_url($title, $sel_app)."'>" . _("Help") . "</a></td>";
+				}
+				echo "<td class=logoutBarRight><a href='$local_path_to_root/access/logout.php?'>" . _("Logout") . "</a>";
+				echo "</td></tr></table>";
+			}
+			echo "</td></tr></table>";
+
+			if ($title && !$no_menu && !$is_index)
+			{
+				echo "<center><table width='100%'><tr><td width='100%' class='titletext'>$title</td></tr></table></center>";
+			}
+
+			if (!$is_index)
+				echo "<br>";
+
 		}
 
-		function menu_footer(&$menu) 
+		function menu_footer($no_menu, $is_index)
 		{
+			global $version, $allow_demo_mode, $app_title, $power_url, $power_by, $path_to_root;
+			include_once($path_to_root . "/includes/date_functions.inc");
+
+			if ($no_menu == false)
+			{
+				if ($is_index)
+					echo "<table class=bottomBar>\n";
+				else
+					echo "<table class=bottomBar2>\n";
+				echo "<tr>";
+				if (isset($_SESSION['wa_current_user']))
+					echo "<td class=bottomBarCell>" . Today() . " | " . Now() . "</td>\n";
+				echo "</tr></table>\n";
+			}
+			echo "</td></tr></table></td>\n";
+			echo "</table>\n";
+			if ($no_menu == false)
+			{
+				echo "<table align='center' id='footer'>\n";
+				echo "<tr>\n";
+				echo "<td align='center' class='footer'><a target='_blank' href='$power_url'><font color='#ffffff'>$app_title $version - " . _("Theme:") . " " . user_theme() . "</font></a></td>\n";
+				echo "</tr>\n";
+				echo "<tr>\n";
+				echo "<td align='center' class='footer'><a target='_blank' href='$power_url'><font color='#ffff00'>$power_by</font></a></td>\n";
+				echo "</tr>\n";
+				if ($allow_demo_mode==true)
+				{
+					echo "<tr>\n";
+					//echo "<td><br><div align='center'><a href='http://sourceforge.net'><img src='http://sourceforge.net/sflogo.php?group_id=89967&amp;type=5' alt='SourceForge.net Logo' width='210' height='62' border='0' align='middle' /></a></div></td>\n";
+					echo "</tr>\n";
+				}
+				echo "</table><br><br>\n";
+			}
 		}
 
-		function display_applications(&$waapp) 
+		function display_applications(&$waapp)
 		{
 
-			$selected_app = &$waapp->get_selected_application();
+			$selected_app = $waapp->get_selected_application();
 
-			foreach ($selected_app->modules as $module) 
+			foreach ($selected_app->modules as $module)
 			{
 				// image
 				echo "<tr>";
@@ -37,17 +120,16 @@
 				echo "</td></tr><tr>";
 				echo "<td class='menu_group_items'>";
 
-				foreach ($module->lappfunctions as $appfunction) 
+				foreach ($module->lappfunctions as $appfunction)
 				{
 					if ($_SESSION["wa_current_user"]->can_access_page($appfunction->access))
 						echo "<a href='$appfunction->link'> " . $appfunction->label . "</a><br>";
 				}
 				echo "</td>";
-				//echo "\nOA_current_user name = " . $_SESSION["wa_current_user"]->username;
-				if (sizeof($module->rappfunctions) > 0) 
+				if (sizeof($module->rappfunctions) > 0)
 				{
 					echo "<td width='50%' class='menu_group_items'>";
-					foreach ($module->rappfunctions as $appfunction) 
+					foreach ($module->rappfunctions as $appfunction)
 					{
 						if ($_SESSION["wa_current_user"]->can_access_page($appfunction->access))
 							echo "<a href='$appfunction->link'> " . $appfunction->label . "</a><br>";
