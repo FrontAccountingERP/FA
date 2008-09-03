98d30c9a3e2f7af94530d1c0fdf000a93ab57e04
Joe Hunt <joe.hunt.consulting@gmail.com>
2008-09-03 14:52:18 +0000

    Bug [0000044] Standard Cost error on credit note. Reopened. Stock Movement forgot the credit note.

--- inventory/inquiry/stock_movements.php
+++ inventory/inquiry/stock_movements.php
@@ -19,7 +19,7 @@ if ($use_date_picker)
 page(_("Inventory Item Movement"), false, false, "", $js);
 //------------------------------------------------------------------------------------------------
 
-if(get_post('ShowMoves')) 
+if(get_post('ShowMoves'))
 {
 	$Ajax->activate('doc_tbl');
 }
@@ -134,7 +134,7 @@ while ($myrow = db_fetch($result))
 			$person = $cust_row['name'] . " (" . $cust_row['br_name'] . ")";
 
 	}
-	elseif ($myrow["type"] == 25)
+	elseif ($myrow["type"] == 25 || $myrow['type'] == 21)
 	{
 		// get the supplier name
 		$sql = "SELECT supp_name FROM ".TB_PREF."suppliers WHERE supplier_id = '" . $myrow["person_id"] . "'";
