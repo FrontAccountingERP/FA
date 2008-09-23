4a73532a91b7f067a5107a7021e5984b7d0ed6cb
Joe Hunt <joe.hunt.consulting@gmail.com>
2008-09-23 09:35:52 +0000

    Release 2.0.2

--- install/save.php
+++ install/save.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", "On");
-ini_set("max_execution_time", "120");
+ini_set("max_execution_time", "180");
 
 // Start a session
 if(!defined('SESSION_STARTED'))
