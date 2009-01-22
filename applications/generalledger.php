44f6b641dffa72949fa3399e4d299623a4d2be56
Janusz Dobrowolski <janusz@frontaccounting.eu>
2009-01-22 16:33:28 +0000

    Hot key for quick entries

--- applications/generalledger.php
+++ applications/generalledger.php
@@ -26,7 +26,7 @@
 
 			$this->add_module(_("Maintenance"));
 			$this->add_lapp_function(2, _("Bank &Accounts"),"gl/manage/bank_accounts.php?");
-			$this->add_lapp_function(2, _("Quick Entries"),"gl/manage/gl_quick_entries.php?");
+			$this->add_lapp_function(2, _("&Quick Entries"),"gl/manage/gl_quick_entries.php?");
 			$this->add_lapp_function(2, "","");
 			$this->add_lapp_function(2, _("&Currencies"),"gl/manage/currencies.php?");
 			$this->add_lapp_function(2, _("&Exchange Rates"),"gl/manage/exchange_rates.php?");
