cca5803c1eebf65e6ac5a54af26f1c2883f3a7d3
Joe Hunt <joe.hunt.consulting@gmail.com>
2009-09-19 09:11:28 +0000

    Error msg in Bank Transfer if no Bank Charge Account set.

--- gl/bank_transfer.php
+++ gl/bank_transfer.php
@@ -129,6 +129,11 @@ function check_valid_entries()
 		set_focus('charge');
 		return false;
 	}
+	if (isset($_POST['charge']) && input_num('charge') > 0 && get_company_pref('bank_charge_act') == '') {
+		display_error(_("The Bank Charge Account has not been set in System and General GL Setup."));
+		set_focus('charge');
+		return false;
+	}
 	if (!$Refs->is_valid($_POST['ref'])) 
 	{
 		display_error(_("You must enter a reference."));
