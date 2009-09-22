36b424339d64978875abbca17352370b54249cab
Joe Hunt <joe.hunt.consulting@gmail.com>
2009-09-22 09:10:14 +0000

    Changed copyright notice in installer files to credit the original copyrighter. GPLv3. Updated the install.html and created a doc txt file, about sales quotations.

--- install/index.php
+++ install/index.php
@@ -1,13 +1,10 @@
 <?php
 /**********************************************************************
-    Copyright (C) FrontAccounting, LLC.
-	Released under the terms of the GNU General Public License, GPL, 
-	as published by the Free Software Foundation, either version 3 
-	of the License, or (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+	This installer is based on code from the	
+ 	Website Baker Project <http://www.websitebaker.org/>
+ 	Copyright (C) 2004-2007, Ryan Djurovich.
+ 	The code is released under GPLv3
+ 	modified by FrontAcounting, LLC.
 ***********************************************************************/
 error_reporting(E_ALL);
 ini_set("display_errors", "On");
