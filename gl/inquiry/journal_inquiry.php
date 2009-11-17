074f5b55ae369597c70953bc7f9324cc491a6a7e
Joe Hunt <joe.hunt.consulting@gmail.com>
2009-11-17 21:09:54 +0000

    Moved payment terms in documents for better view. Increased space for legal text. Changed sorting in get_gl_transactions to sort by tran_date, counter Changed period in Journal Entries to one month instead of one year.

--- gl/inquiry/journal_inquiry.php
+++ gl/inquiry/journal_inquiry.php
@@ -45,7 +45,7 @@ start_row();
 ref_cells(_("Reference:"), 'Ref', '',null, _('Enter reference fragment or leave empty'));
 
 journal_types_list_cells(_("Type:"), "filterType");
-date_cells(_("From:"), 'FromDate', '', null, 0, 0, -1);
+date_cells(_("From:"), 'FromDate', '', null, 0, -1, 0);
 date_cells(_("To:"), 'ToDate');
 
 check_cells( _("Show closed:"), 'AlsoClosed', null);
