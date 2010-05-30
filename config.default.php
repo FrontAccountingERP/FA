b5c5d915d8ce6e85605dce6efc5d2b61ea02f3ee
Joe Hunt <joe.hunt.consulting@gmail.com>
2010-05-30 11:35:02 +0000

    Global variabel, $def_print_destination, config.php can be set to 0 or 1 Copy line memo to next line in journal entries and bank payments/deposits

--- config.default.php
+++ config.default.php
@@ -80,6 +80,9 @@ if (!isset($path_to_root) || isset($_GET['path_to_root']) || isset($_POST['path_
 	/* show users online discretely in the footer */
 	$show_users_online = 0;
 
+	/* default print destination. 0 = PDF/Printer, 1 = Excel */
+	$def_print_destination = 0;
+
  	// Wiki context help configuration
  	// If your help wiki use translated page titles uncomment next line
  	// $old_style_help = 1; // this setting is depreciated and subject to removal in next FA versions
