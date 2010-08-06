0e219ded989a72cbd5eaf21c42970aa777ce770c
Janusz Dobrowolski <janusz@frontaccounting.eu>
2010-08-06 14:50:32 +0000

    Added chart packages installation.

--- applications/setup.php
+++ applications/setup.php
@@ -70,7 +70,9 @@ class setup_app extends application
 		$this->add_rapp_function(2, _("Install/Activate &Extensions"),
 			"admin/inst_module.php?", 'SA_CREATEMODULES');
 		$this->add_rapp_function(2, _("Install/Activate &Themes"),
-			"admin/inst_theme.php?", 'SA_CREATEMODULES');
+			"admin/inst_module.php?", 'SA_CREATEMODULES');
+		$this->add_rapp_function(2, _("Install/Activate &Chart of Accounts"),
+			"admin/inst_chart.php?", 'SA_CREATEMODULES');
 		$this->add_rapp_function(2, _("Software &Upgrade"),
 			"admin/inst_upgrade.php?", 'SA_SOFTWAREUPGRADE');
 
