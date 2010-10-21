6bbb6897f5a2ffc9d9e849a97fdca5f3aa6f1c1d
Janusz Dobrowolski <janusz@frontaccounting.eu>
2010-10-21 14:39:11 +0000

    Improper amount sign in bank payment view fixed.

--- gl/view/gl_payment_view.php
+++ gl/view/gl_payment_view.php
@@ -62,7 +62,7 @@ start_row();
 label_cells(_("From Bank Account"), $from_trans['bank_account_name'], "class='tableheader2'");
 if ($show_currencies)
 	label_cells(_("Currency"), $from_trans['bank_curr_code'], "class='tableheader2'");
-label_cells(_("Amount"), number_format2($from_trans['amount'], user_price_dec()), "class='tableheader2'", "align=right");
+label_cells(_("Amount"), number_format2(-$from_trans['amount'], user_price_dec()), "class='tableheader2'", "align=right");
 label_cells(_("Date"), sql2date($from_trans['trans_date']), "class='tableheader2'");
 end_row();
 start_row();
