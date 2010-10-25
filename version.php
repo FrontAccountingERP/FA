5dc52342439c424585dec995bafcfbf0ccae829f
Janusz Dobrowolski <janusz@frontaccounting.eu>
2010-10-25 08:48:02 +0000

    Version 2.3RC3

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ $db_version = "2.3rc";
 
 // application version - can be overriden in config.php
 if (!isset($version))
-	$version 		= "2.3RC2";
+	$version 		= "2.3RC3";
 
 //======================================================================
 // Extension packages repository settings 
