652749ddedebe179eb3e3ec7cc55b0f39539b98b
Janusz Dobrowolski <janusz@frontaccounting.eu>
2010-10-30 13:34:18 +0000

    Normalized add_domain() params  in native phpgettext classes

--- includes/lang/gettext.php
+++ includes/lang/gettext.php
@@ -60,6 +60,7 @@ function is_error($err) {
 class gettext_native_support 
 {
     var $_interpolation_vars = array();
+    var $domain_path;
 
     /**
      * Set gettext language code.
@@ -125,6 +126,8 @@ class gettext_native_support
      */
     function add_domain($domain, $path=false, $version='')
     {
+        if ($path === false) 
+	        $path = $this->domain_path;
         if ($path === false) 
 	        $path = "./locale";
 	    if ($domain == "")
@@ -293,8 +296,13 @@ class gettext_php_support extends gettext_native_support
      * @param string $path optional -- Repository path
      * @throws GetText_Error
      */
-    function add_domain($domain, $path = "./locale/", $version ='')
+    function add_domain($domain, $path = false, $version ='')
     {   
+        if ($path === false) 
+	      $path = $this->domain_path;
+        if ($path === false) 
+	        $path = "./locale";
+
     	if ($version) {
 			$domain .= '-'.$version;
 		}
