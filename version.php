b6f74be8dc65c5a9c6051e15631d365fae7ba54d
Joe Hunt <joe.hunt.consulting@gmail.com>
2011-03-31 22:54:18 +0200

    Version 2.3.3. All install language templates updated.

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ $db_version = "2.3rc";
 
 // application version - can be overriden in config.php
 if (!isset($version))
-	$version 		= "2.3.2";
+	$version 		= "2.3.3";
 
 //======================================================================
 // Extension packages repository settings 
