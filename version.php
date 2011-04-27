01c5bb6a028462081fe47b9c8dcf46d46d4c4f4a
Joe Hunt <joe.hunt.consulting@gmail.com>
2011-04-27 10:58:11 +0200

    Release 2.3.4

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ $db_version = "2.3rc";
 
 // application version - can be overriden in config.php
 if (!isset($version))
-	$version 		= "2.3.3";
+	$version 		= "2.3.4";
 
 //======================================================================
 // Extension packages repository settings 
