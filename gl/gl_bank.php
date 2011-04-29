2d23368483326fc5229a37e715de0769dcee883a
Janusz Dobrowolski <janusz@frontaccouting.eu>
2011-04-29 13:52:59 +0200

    Additional checks on bank transaction entry/delete to avoid overdraft in following transactions.

--- gl/gl_bank.php
+++ gl/gl_bank.php
@@ -226,13 +226,21 @@ if (isset($_POST['Process']))
 
 	$limit = get_bank_account_limit($_POST['bank_account'], $_POST['date_']);
 
-	if ($limit != null && (($limit - $_SESSION['pay_items']->original_amount) < $_SESSION['pay_items']->gl_items_total()))
+	$amnt_chg = -$_SESSION['pay_items']->gl_items_total()-$_SESSION['pay_items']->original_amount;
+
+	if ($limit != null && ($limit + $amnt_chg < 0))
 	{
 		display_error(sprintf(_("The total bank amount exceeds allowed limit (%s)."), price_format($limit-$_SESSION['pay_items']->original_amount)));
 		set_focus('code_id');
 		$input_error = 1;
 	}
+	if ($trans = check_bank_account_history($amnt_chg, $_POST['bank_account'], $_POST['date_'])) {
 
+		display_error(sprintf(_("The bank transaction would result in exceed of authorized overdraft limit for transaction: %s #%s on %s."),
+			$systypes_array[$trans['type']], $trans['trans_no'], sql2date($trans['trans_date'])));
+		set_focus('amount');
+		$input_error = 1;
+	}
 	if (!$Refs->is_valid($_POST['ref']))
 	{
 		display_error( _("You must enter a reference."));
