b1c9712c6042b26d43ce9c78c77d078ef1906a02
Janusz Dobrowolski <janusz@frontaccouting.eu>
2012-02-16 10:40:33 +0100

    [0001174] Adding bank account tnumber to customer branch record.

--- sql/alter2.4.sql
+++ sql/alter2.4.sql
@@ -33,3 +33,5 @@ UPDATE `0_tax_group_items` tgi
 	WHERE tgi.rate=(SELECT 0_tax_types.rate FROM 0_tax_types, 0_tax_groups 
 		WHERE tax_shipping=1 AND tgi.tax_group_id=0_tax_groups.id AND tgi.tax_type_id=0_tax_types.id);
 ALTER TABLE `0_tax_groups` DROP COLUMN `tax_shipping`;
+
+ALTER TABLE `0_cust_branch` ADD COLUMN `bank_account` varchar(60) DEFAULT NULL AFTER `notes`;
