bda6054270dff1d4c791c8068ff3c92cc4442fa4
Joe Hunt <joe.hunt.consulting@gmail.com>
2012-07-16 23:29:50 +0200

    Purchases - Direct Invoice - Database Error in some mysql configurations

--- purchasing/po_entry_items.php
+++ purchasing/po_entry_items.php
@@ -454,7 +454,7 @@ function handle_commit_order()
 			$supp = get_supplier($cart->supplier_id);
 			$inv->tax_group_id = $supp['tax_group_id'];
 //			$inv->ov_discount 'this isn't used at all'
-			$inv->ov_amount = $inv->ov_gst = 0;
+			$inv->ov_amount = $inv->ov_gst = $inv->ov_discount = 0;
 			
 			foreach($cart->line_items as $key => $line) {
 				$inv->add_grn_to_trans($line->grn_item_id, $line->po_detail_rec, $line->stock_id,
