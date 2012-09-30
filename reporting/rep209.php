cecd73530c52428aa91ade79693d1e1b03531451
Joe Hunt <joe.hunt.consulting@gmail.com>
2012-10-01 01:12:38 +0200

    Improved headers in printing Purchase Orders.

--- reporting/rep209.php
+++ reporting/rep209.php
@@ -34,7 +34,7 @@ print_po();
 function get_po($order_no)
 {
    	$sql = "SELECT ".TB_PREF."purch_orders.*, ".TB_PREF."suppliers.supp_name,  "
-   		.TB_PREF."suppliers.supp_account_no,".TB_PREF."suppliers.tax_included,
+   		.TB_PREF."suppliers.supp_account_no,".TB_PREF."suppliers.tax_included,".TB_PREF."suppliers.gst_no AS tax_id,
    		".TB_PREF."suppliers.curr_code, ".TB_PREF."suppliers.payment_terms, ".TB_PREF."locations.location_name,
    		".TB_PREF."suppliers.address, ".TB_PREF."suppliers.contact, ".TB_PREF."suppliers.tax_group_id
 		FROM ".TB_PREF."purch_orders, ".TB_PREF."suppliers, ".TB_PREF."locations
