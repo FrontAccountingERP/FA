bd9f5ffaa36f9bbd54498bc9ae264fe53b0a86ba
Janusz Dobrowolski <janusz@frontaccouting.eu>
2013-01-16 13:22:26 +0100

    Small fixes in attachments (check for non existent trasactions, proper page refresh after attachment)

--- admin/attachments.php
+++ admin/attachments.php
@@ -19,6 +19,7 @@ include_once($path_to_root . "/includes/date_functions.inc");
 include_once($path_to_root . "/includes/ui.inc");
 include_once($path_to_root . "/includes/data_checks.inc");
 include_once($path_to_root . "/admin/db/attachments_db.inc");
+include_once($path_to_root . "/admin/db/transactions_db.inc");
 
 if (isset($_GET['vw']))
 	$view_id = $_GET['vw'];
@@ -81,8 +82,8 @@ if (isset($_GET['trans_no']))
 
 if ($Mode == 'ADD_ITEM' || $Mode == 'UPDATE_ITEM')
 {
-	if (!$_POST['trans_no'])
-		display_error(_("No transaction has been selected."));
+	if (!transaction_exists($_POST['filterType'], $_POST['trans_no']))
+		display_error(_("Selected transaction does not exists."));
 	elseif ($Mode == 'ADD_ITEM' && (!isset($_FILES['filename']) || $_FILES['filename']['size'] == 0))
 		display_error(_("Select attachment file."));
 	else {
@@ -130,7 +131,7 @@ if ($Mode == 'ADD_ITEM' || $Mode == 'UPDATE_ITEM')
 			display_notification(_("Attachment has been updated.")); 
 		}
 	}
-	refresh_pager('trans_table');
+	refresh_pager('trans_tbl');
 	$Ajax->activate('_page_body');
 	$Mode = 'RESET';
 }
