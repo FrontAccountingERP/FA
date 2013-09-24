f840050289f0dd5f6c8ffc4fc6c4b8199bb86e01
Janusz Dobrowolski <janusz@frontaccounting.eu>
2013-09-24 13:24:58 +0200

    Bank Payment/Bank Deposit: removed sparse sign from settled amount input, fixed sign in AR/AP record; Fixed amounts displayed in header for customer/supplier bank payments.

--- gl/gl_bank.php
+++ gl/gl_bank.php
@@ -208,8 +208,9 @@ function create_cart($type, $trans_no)
 }
 //-----------------------------------------------------------------------------------------------
 
-if (isset($_POST['Process']))
+function check_trans()
 {
+	global $Refs;
 
 	$input_error = 0;
 
@@ -279,11 +280,15 @@ if (isset($_POST['Process']))
 	if (!db_has_currency_rates(get_bank_account_currency($_POST['bank_account']), $_POST['date_'], true))
 		$input_error = 1;
 
-	if ($input_error == 1)
-		unset($_POST['Process']);
+	if (in_array(get_post('PayType'), array(PT_SUPPLIER, PT_CUSTOMER)) && (input_num('settled_amount') <= 0)) {
+		display_error(_("Settled amount have to be positive number."));
+		set_focus('person_id');
+		$input_error = 1;
+	}
+	return $input_error;
 }
 
-if (isset($_POST['Process']))
+if (isset($_POST['Process']) && !check_trans())
 {
 	begin_transaction();
 
