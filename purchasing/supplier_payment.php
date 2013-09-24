26a98f159b27fb2eda00c4f69bd007bea32333c8
Janusz Dobrowolski <janusz@frontaccounting.eu>
2013-09-24 17:45:01 +0200

    Supplier Payment: small fix in fixed ui translation.

--- purchasing/supplier_payment.php
+++ purchasing/supplier_payment.php
@@ -303,7 +303,7 @@ start_form();
 
 	if ($bank_currency != $supplier_currency) 
 	{
-		amount_row("Bank Amount:", 'bank_amount', null, '', $bank_currency, 2);
+		amount_row(_("Bank Amount:"), 'bank_amount', null, '', $bank_currency, 2);
 	}
 
 	amount_row(_("Bank Charge:"), 'charge', null, '', $bank_currency);
