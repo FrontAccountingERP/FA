b07ff0ac025d4f4510a7de0808cd2da886c1c7c6
Janusz Dobrowolski <janusz@frontaccounting.eu>
2013-10-28 09:35:17 +0100

    Allocate Customer Payment or Credit Note, Allocate Supplier Payment or Credit Note: settled amount displayed in header.

--- sales/allocations/customer_allocate.php
+++ sales/allocations/customer_allocate.php
@@ -54,11 +54,12 @@ function edit_allocations_for_transaction($type, $trans_no)
     display_heading2(_("Date:") . " <b>" . $cart->date_ . "</b>");
    	display_heading2(_("Total:"). " <b>" . price_format($cart->bank_amount).' '.$cart->currency."</b>");
 
-	if ($cart->currency != $cart->person_curr)
+	if (floatcmp($cart->bank_amount, $cart->amount))
 	{
-	    $total = _("Total in clearing currency:") . " <b>" . price_format($cart->amount)."</b>"
-	    	. sprintf(" %s (%s %s/%s)", $cart->person_curr, exrate_format($cart->bank_amount/$cart->amount), $cart->currency, $cart->person_curr);
-    	display_heading2($total);
+	    $total = _("Amount ot be settled:") . " <b>" . price_format($cart->amount).' '.$cart->person_curr."</b>";
+		if ($cart->currency != $cart->person_curr)
+    		$total .= sprintf(" (%s %s/%s)",  exrate_format($cart->bank_amount/$cart->amount), $cart->currency, $cart->person_curr);
+	   	display_heading2($total);
 	}
 
     echo "<br>";
