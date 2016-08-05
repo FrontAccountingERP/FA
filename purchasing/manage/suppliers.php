FrontAccountingERP/FA

commit 99df2fbc2f147b3a1e20a4cdfa70c2720e67e4cb

Rerun. Tabbed dialogs allowed non access pages.

purchasing/manage/suppliers.php
@@ -309,7 +309,7 @@ tabbed_content_start('tabs', array(
 		'settings' => array(_('&General settings'), $supplier_id),
 		'contacts' => array(_('&Contacts'), $supplier_id),
 		'transactions' => array(_('&Transactions'), (user_check_access('SA_SUPPTRANSVIEW') ? $supplier_id : null)),
-		'orders' => array(_('Purchase &Orders'), $supplier_id),
+		'orders' => array(_('Purchase &Orders'), (user_check_access('SA_SUPPTRANSVIEW') ? $supplier_id : null)),,
 	));
 	
 	switch (get_post('_tabs_sel')) {

sales/manage/customers.php
@@ -350,7 +350,7 @@ tabbed_content_start('tabs', array(
 		'settings' => array(_('&General settings'), $selected_id),
 		'contacts' => array(_('&Contacts'), $selected_id),
 		'transactions' => array(_('&Transactions'), (user_check_access('SA_SALESTRANSVIEW') ? $selected_id : null)),
-		'orders' => array(_('Sales &Orders'), $selected_id),
+		'orders' => array(_('Sales &Orders'), (user_check_access('SA_SALESTRANSVIEW') ? $selected_id : null)),
 	));
 	
 	switch (get_post('_tabs_sel')) {

commit db34cf90a8fb4ba6e9c2c956b0580578854f43ab

Fix #16: Manage Suppliers
- Fix error introduced in 99df2fbc2f147b3a1e20a4cdfa70c2720e67e4cb

purchasing/manage/suppliers.php
@@ -309,7 +309,7 @@ tabbed_content_start('tabs', array(
 		'settings' => array(_('&General settings'), $supplier_id),
 		'contacts' => array(_('&Contacts'), $supplier_id),
 		'transactions' => array(_('&Transactions'), (user_check_access('SA_SUPPTRANSVIEW') ? $supplier_id : null)),
-		'orders' => array(_('Purchase &Orders'), (user_check_access('SA_SUPPTRANSVIEW') ? $supplier_id : null)),,
+		'orders' => array(_('Purchase &Orders'), (user_check_access('SA_SUPPTRANSVIEW') ? $supplier_id : null)),
 	));
 	
 	switch (get_post('_tabs_sel')) {