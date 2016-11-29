840f9a44261cdf4ee95a5cc9a29314b9ddb06b9c
Joe Hunt <joe.hunt.consulting@gmail.com>
2016-11-29 16:20:32 +0100

    Fixed A Bug in Item Sales Summary Report

--- reporting/rep309.php
+++ reporting/rep309.php
@@ -51,7 +51,7 @@ function getTransactions($category, $from, $to)
 		AND trans.tran_date<='$to'
 		AND line.quantity<>0
 		AND item.mb_flag <>'F'
-		AND line.debtor_trans_type = ".ST_SALESINVOICE;
+		AND (line.debtor_trans_type = ".ST_SALESINVOICE." OR line.debtor_trans_type = ".ST_CUSTCREDIT.")";
 		if ($category != 0)
 			$sql .= " AND item.category_id = ".db_escape($category);
 		$sql .= " GROUP BY item.category_id,
