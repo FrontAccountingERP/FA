7657fb038729a6ae3688a98ca3b18a74614dd79f
Joe Hunt <joe.hunt.consulting@gmail.com>
2017-05-19 21:53:28 +0200

    Sales type potential bug in tax_included (persistent on)

--- sales/manage/sales_types.php
+++ sales/manage/sales_types.php
@@ -43,7 +43,7 @@ function can_process()
 
 if ($Mode=='ADD_ITEM' && can_process())
 {
-	add_sales_type($_POST['sales_type'], isset($_POST['tax_included']) ? 1:0,
+	add_sales_type($_POST['sales_type'], check_value('tax_included'),
 	    input_num('factor'));
 	display_notification(_('New sales type has been added'));
 	$Mode = 'RESET';
@@ -54,7 +54,7 @@ if ($Mode=='ADD_ITEM' && can_process())
 if ($Mode=='UPDATE_ITEM' && can_process())
 {
 
-	update_sales_type($selected_id, $_POST['sales_type'], isset($_POST['tax_included']) ? 1:0,
+	update_sales_type($selected_id, $_POST['sales_type'], check_value('tax_included'),
 	     input_num('factor'));
 	display_notification(_('Selected sales type has been updated'));
 	$Mode = 'RESET';
