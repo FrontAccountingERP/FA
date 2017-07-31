480e5f57b0cd2f439b0f8f8ea7cfe9f630b8b4e9
Joe Hunt <joe.hunt.consulting@gmail.com>
2017-07-31 10:35:03 +0200

    db_version was changed by mistake. Should remain at 2.4.1.

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 //
 
 // Versions used by source/database version compatibility checks. Do not change.
-$db_version = "2.4.2";
+$db_version = "2.4.1";
 $src_version = "2.4.2";
 
 // application version - can be overriden in config.php
