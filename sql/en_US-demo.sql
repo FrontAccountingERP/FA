0fcc421f3bc76dbaf2331a11804d804b08f37545
Janusz Dobrowolski <janusz@frontaccounting.eu>
2017-10-27 12:30:06 +0200

    Fixed supp_trans primary key.

--- sql/en_US-demo.sql
+++ sql/en_US-demo.sql
@@ -2069,7 +2069,7 @@ CREATE TABLE IF NOT EXISTS `0_supp_trans` (
   `rate` double NOT NULL DEFAULT '1',
   `alloc` double NOT NULL DEFAULT '0',
   `tax_included` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`type`,`trans_no`),
+  PRIMARY KEY (`type`,`trans_no`,`debtor_no`),
   KEY `supplier_id` (`supplier_id`),
   KEY `tran_date` (`tran_date`)
 ) ENGINE=InnoDB;
