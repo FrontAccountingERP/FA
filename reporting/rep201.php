d25e06f889d2c81871fcb6edb42e53e2ea316601
Joe Hunt <joe.hunt.consulting@gmail.com>
2018-08-18 08:39:57 +0200

    Fixed bug in Report Supplier Balances.

--- reporting/rep201.php
+++ reporting/rep201.php
@@ -29,17 +29,25 @@ print_supplier_balances();
 
 function get_open_balance($supplier_id, $to)
 {
-	$to = date2sql($to);
+    if ($to)
+        $to = date2sql($to);
 
-    $sql = "SELECT
-    	SUM(IF(type IN(".ST_SUPPINVOICE.",".ST_BANKDEPOSIT."), (ov_amount + ov_gst + ov_discount), 0)) AS charges,
-    	SUM(IF(type NOT IN(".ST_SUPPINVOICE.",".ST_BANKDEPOSIT."), (ov_amount + ov_gst + ov_discount), 0)) AS credits,
-		SUM(IF(type NOT IN(".ST_SUPPINVOICE.",".ST_BANKDEPOSIT."),alloc * -1, alloc)) AS Allocated,
-		SUM(IF(type IN(".ST_SUPPINVOICE.",".ST_BANKDEPOSIT."), (ov_amount + ov_gst + ov_discount - alloc),
-				(ov_amount + ov_gst + ov_discount + alloc))) AS OutStanding
-		FROM ".TB_PREF."supp_trans
-    	WHERE tran_date < '$to'
-		AND supplier_id = '$supplier_id' GROUP BY supplier_id";
+    $sql = "SELECT SUM(IF(t.type = ".ST_SUPPINVOICE." OR (t.type IN (".ST_JOURNAL." , ".ST_BANKDEPOSIT.") AND t.ov_amount>0),
+        -abs(t.ov_amount + t.ov_gst + t.ov_discount), 0)) AS charges,";
+
+    $sql .= "SUM(IF(t.type != ".ST_SUPPINVOICE." AND NOT(t.type IN (".ST_JOURNAL." , ".ST_BANKDEPOSIT.") AND t.ov_amount>0),
+        abs(t.ov_amount + t.ov_gst + t.ov_discount) * -1, 0)) AS credits,";
+
+    $sql .= "SUM(IF(t.type != ".ST_SUPPINVOICE." AND NOT(t.type IN (".ST_JOURNAL." , ".ST_BANKDEPOSIT.")), t.alloc * -1, t.alloc)) 
+        AS Allocated,";
+
+    $sql .= "SUM(IF(t.type = ".ST_SUPPINVOICE.", 1, -1) *
+        (abs(t.ov_amount + t.ov_gst + t.ov_discount) - abs(t.alloc))) AS OutStanding
+        FROM ".TB_PREF."supp_trans t
+        WHERE t.supplier_id = ".db_escape($supplier_id);
+    if ($to)
+        $sql .= " AND t.tran_date < '$to'";
+    $sql .= " GROUP BY supplier_id";
 
     $result = db_query($sql,"No transactions were returned");
     return db_fetch($result);
