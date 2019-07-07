a9ef5cd6f4e00b4f287f113de9f865b6ea439b8e
Janusz Dobrowolski <janusz@frontaccounting.eu>
2019-07-07 16:24:23 +0200

    Changed purch_order.requisition_no field name to supp_reference, cleanups in purch_order class.

--- sql/alter2.5.sql
+++ sql/alter2.5.sql
@@ -1,4 +1,6 @@
 # reliable cost change log in stock_moves
 ALTER TABLE `0_stock_moves` CHANGE COLUMN `standard_cost` `unit_cost` double NOT NULL DEFAULT '0';
 ALTER TABLE `0_debtor_trans_details` CHANGE COLUMN `standard_cost` `unit_cost` double NOT NULL DEFAULT '0';
+#naming cleanups
+ALTER TABLE `0_purch_orders` CHANGE COLUMN `requisition_no` `supp_reference` tinytext;
 
