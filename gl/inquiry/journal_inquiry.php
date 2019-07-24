fe62f4fb15a36d7c62b2aa16acafdf8a09e4c7dc
Joe Hunt <joe.hunt.consulting@gmail.com>
2019-07-24 15:19:38 +0200

    Journal Inquiry Dates should follow the User Preference Transaction Days. Fixed.

--- gl/inquiry/journal_inquiry.php
+++ gl/inquiry/journal_inquiry.php
@@ -45,7 +45,7 @@ start_row();
 ref_cells(_("Reference:"), 'Ref', '',null, _('Enter reference fragment or leave empty'));
 
 journal_types_list_cells(_("Type:"), "filterType");
-date_cells(_("From:"), 'FromDate', '', null, 0, -1, 0);
+date_cells(_("From:"), 'FromDate', '', null, -user_transaction_days());
 date_cells(_("To:"), 'ToDate');
 
 end_row();
