1ca1041c196876505cb9b591afbba34ada239682
Joe <joe.hunt.consulting@gmail.com>
2022-11-12 14:04:03 +0100

    Another rerun. Not translatable string in rep108. Fixed and improved. New empty.po.

--- reporting/rep108.php
+++ reporting/rep108.php
@@ -175,8 +175,12 @@ function print_statements()
 		for ($i = 0; $i < 5; $i++)
 			$rep->TextWrap($col[$i], $rep->row, $col[$i + 1] - $col[$i], $str2[$i], 'right');
 		if ($email == 1)
-			$rep->End($email, _("Statement") . " " . _("as of") . " " . sql2date($date));
-
+		{
+            if (($CustomerRecord["Balance"]) != ($CustomerRecord["Balance"] - $CustomerRecord["Due"]))
+                $rep->End($email, _("Statement") . " " . _("as of") . " " . sql2date($date) . " " . _("from") . " " . get_company_pref('coy_name'));
+            else
+                display_notification(sprintf(_("Customer %s has no overdue debs. No e-mail is send."), $myrow["DebtorName"]));       
+        }
 	}
 
     if (!isset($rep))
