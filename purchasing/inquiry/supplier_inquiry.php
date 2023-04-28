36823b0fc61d0b2bcffb5d6fbdcc8b1714e8c9c9
Joe <joe.hunt.consulting@gmail.com>
2023-04-28 14:53:39 +0200

    Bug 5697: In Supplier Transaction Inquiry, Aging balances are Incorrect. Fixed.

--- purchasing/inquiry/supplier_inquiry.php
+++ purchasing/inquiry/supplier_inquiry.php
@@ -158,7 +158,7 @@ div_start('totals_tbl');
 
 if ($_POST['supplier_id'] != "" && $_POST['supplier_id'] != ALL_TEXT)
 {
-	$supplier_record = get_supplier_details(get_post('supplier_id'), get_post('TransToDate'), false);
+	$supplier_record = get_supplier_details(get_post('supplier_id'), get_post('TransToDate'), true);
     display_supplier_summary($supplier_record);
 }
 div_end();
